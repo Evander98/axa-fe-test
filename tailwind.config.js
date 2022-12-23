6e1ba2688df3180cc8a1daba6d8b35f8fa274bfd
Evander98 <christrevand@gmail.com>
2022-12-23 17:43:08 +0800

    feat: add css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: '#0E47C0',
+      secondary: '#ffd72b'
+    },
     extend: {},
   },
   plugins: [],
