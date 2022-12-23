2c7f223df660a9cc5fede8c14474a9c1fdb5e85d
Evander98 <christrevand@gmail.com>
2022-12-23 17:31:43 +0800

    feat: add layout

---
+++ components/layouts/index.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import React, { useState } from "react";
+import Drawer from "../Drawer";
+import Header from "../Header";
+
+const index = ({ children, title }) => {
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Header />
+      <div className="flex">
+        <div className={toggle ? "" : "mr-14"}>
+          <Drawer toggle={toggle} setToggle={setToggle} />
+        </div>
+        <div className="p-8 w-full">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
