9ce16c0806deb0d584b4c3ebdccd0411b184f23c
Evander98 <christrevand@gmail.com>
2022-12-23 17:35:27 +0800

    feat: add header

---
+++ components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-[80px] w-full shadow-lg flex items-center px-8'>
+      <p className='headline--large'>AXA FE Assessment</p>
+    </div>
+  )
+}
+
+export default Header
