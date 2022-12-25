faef2ff598845dc560255007500f25a9624d1b15
Evander98 <christrevand@gmail.com>
2022-12-26 02:16:21 +0800

    fix: update card list components

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { BiEditAlt, BiTrash } from "react-icons/bi";
 
-const Card = ({ data, link }) => {
-  let router = useRouter()
+const Card = ({ data, link, isDelete, isEdit, onDelete, onEdit }) => {
+  let router = useRouter();
   return (
-    <div className="w-[350px] border-2 rounded-xl p-4 cursor-pointer" onClick={() => router.push(`${link}/${data?.id}`)}>
-      <p className="font-bold truncate mb-2">{data?.title}</p>
-      <p className="four-line">{data?.body}</p>
-      {
-        data?.thumbnailUrl && <img src={data.thumbnailUrl} alt="Image" className="m-auto"/>
-      }
+    <div className="w-[350px] border-2 rounded-xl p-4 cursor-pointer flex justify-between">
+      <div
+        className="w-[90%]"
+        onClick={() => router.push(`${link}/${data?.id}`)}
+      >
+        <p className="font-bold truncate mb-2">{data?.title}</p>
+        <p className="four-line">{data?.body}</p>
+        {data?.thumbnailUrl && (
+          <img src={data.thumbnailUrl} alt="Image" className="m-auto" />
+        )}
+      </div>
+      <div>
+        {isDelete && (
+          <BiTrash
+            size={30}
+            className="mb-4 hover:text-primary cursor-pointer"
+            onClick={onDelete}
+          />
+        )}
+        {isEdit ? (
+          <BiEditAlt
+            size={30}
+            className="hover:text-primary cursor-pointer"
+            onClick={onEdit}
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
--- components/List/Card.jsx
+++ components/List/Card.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Card from "../Card";
 
-const CardList = ({ data, meta }) => {
+const CardList = ({ data, meta, isDelete, id, onDelete, onEdit }) => {
   return (
     <div>
-      <p className="headline--large mb-4">{meta.text}</p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid-flow-row gap-4">
-        {data?.map((item, index) => (
-          <Card data={item} key={index} link={meta.link}/>
-        ))}
-      </div>
+      {meta.text && <p className="headline--large mb-4">{meta.text}</p>}
+      {data.length ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid-flow-row gap-4">
+          {data?.map((item, index) => (
+            <Card
+              data={item}
+              key={index}
+              link={meta?.link}
+              isDelete={isDelete}
+              isEdit={item[id] === 501 ? true : false}
+              onDelete={() => onDelete(index)}
+              onEdit={() => onEdit(index)}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">No data</p>
+      )}
     </div>
   );
 };
